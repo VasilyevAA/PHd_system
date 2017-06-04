81e82a4d82707bed87eb010e4eb2b76c26cdbf76
VAA <sasha19.10.1993@gmail.com>
2017-06-04 20:08:14 +0300

    Did creater

--- test_create.py
+++ test_create.py
@@ -8,28 +8,44 @@ import os
 import threading
 
 
-path1 = ['dir1/test1.1','dir1/test1.2']
-path2 = ['dir2/test2.1','dir2/test2.2']
-path3 = ['dir3/test3.1','dir3/test3.2']
+path1 = ['1\\test1.1','1\\test1.2']
+path2 = ['2\\test2.1','2\\test2.2']
+path3 = ['3\\test3.1','3\\test3.2']
 
 allProcesses = []
 pathAll = [path1, path2, path3]
 
-def createAndWrite(path):
-    i=1
-    for pt in path:
-        f = open(os.getcwd()+'/'+pt, 'r+')
-        print(os.getcwd()+'/'+pt)
-        for x in range(0,10):
-            f.write('%s    %s file for process %s: %s\n'%(datetime.datetime.now(), i, os.getpid(), x))
+class WriteData(threading.Thread):
+    def __init__(self, arr):
+        threading.Thread.__init__(self)
+        self.arr = arr
 
-        i+=1
-        time.sleep(5)
+    def run(self):
+        i = 1
+        for pt in self.arr:
+            print(os.getcwd() + '/' + pt)
+            lol = os.getcwd()
+            for x in range(0, 4):
+                f = open(str(lol) + '/' + pt, 'a+')
+                print(f.read())
+                f.seek(len(f.read()))
+                f.write('%s    %s file for process %s: %s\n' % (datetime.datetime.now(), i, self.getName(), x))
+                f.close()
+                time.sleep(1)
 
+            i += 1
+            time.sleep(5)
 
-if __name__ == "__main__":
-    for path in pathAll:
-        #p = multiprocessing.Process(target=createAndWrite, args=path)
-        #allProcesses.append(p)
-        #p.start()
-    #print('danger                         :'+os.getpid())
+for x in range(0, 3):
+    t = str(os.getcwd()) + '/' + str(x)
+    try:
+        os.makedirs(t)
+    except OSError:
+        print('папка существует:'+t)
+
+for path in pathAll:
+    p = WriteData(path)
+    allProcesses.append(p)
+    p.start()
+
+print('danger                         :'+str(os.getpid()))
